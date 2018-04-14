06aafd68becb4863d2d8f61494ff6ef088118246
libowei1213 <libowei123123@qq.com>
2018-04-14 19:37:09 +0800

    4.14

--- download_courseware.py
+++ download_courseware.py
@@ -1,12 +1,10 @@
 # coding=utf-8
 
-import requests
+from requests_html import HTMLSession
 import re
-from bs4 import BeautifulSoup
 import os
 import sys
 import json
-import time
 import time
 
 
@@ -14,15 +12,12 @@ import time
 def download(url, fileName, className, session):
     # \xa0转gbk会有错
     fileName = fileName.replace(u"\xa0", " ").replace(u"\xc2", "")
-    # 去掉不合法的文件名字符
-    fileName = re.sub(r"[/\\:*\"<>|?]", "", fileName)
-    className = re.sub(r"[/\\:*\"<>|?]", "", className)
 
-    dir = os.getcwd() + "/" + className
-    file = os.getcwd() + "/" + className + "/" + fileName
+    dir = os.path.join(os.getcwd(), className)
+    file = os.path.join(dir, fileName)
     # 没有课程文件夹则创建
     if not os.path.exists(dir):
-        os.mkdir(dir)
+        os.makedirs(dir)
     # 存在该文件，返回
     if os.path.exists(file):
         print("%s已存在，就不下载了" % fileName)
@@ -41,39 +36,58 @@ def errorExit(msg):
 
 def getClass(currentClass, url, session, data):
     if data != None:
-        s = session.post(url, data=data)
+        r = session.post(url, data=data)
     else:
-        s = session.get(url)
-    resourceList = BeautifulSoup(s.text, "html.parser").findAll("tr")
+        r = session.get(url)
 
-    for res in resourceList:
-        if res.find("td") == None:
-            continue
-        if res.find("h4") == None:
-            continue
+    resourceList = r.html.find("tr")
 
-        resUrl = res.find("h4").a.get("href")
-
+    for res in resourceList[2:]:
+        resUrl = res.find("a", first=True).attrs.get("href")
         # 文件夹
-        if res.find("h4").a.get("title") == "打开此文件夹":
-            path = res.find("td", {"headers": "checkboxes"}).input.get("value")
-            data = {'source': '0', 'collectionId': path, 'navRoot': '', 'criteria': '', 'sakai_action': 'doNavigate',
-                    'rt_action': '', 'selectedItemId': '', 'resourceName': ''}
-            # print (path)
-            urlNew = BeautifulSoup(s.text, "html.parser").find("form").get("action")
-            getClass(currentClass, urlNew, session, data)
+        if res.find("a", first=True).attrs.get("title") == "打开此文件夹":
+            path = res.find("a", first=True).attrs.get("onclick")
+            reg = re.compile("Id'\).value='([\s\S]*)';document")
+            match = reg.search(path)
+            if match:
+                path = match.group(1)
+
+                reg = re.compile('name="sakai_csrf_token" value="(.*)"')
+                sakai_csrf_token = reg.search(r.text).group(1)
+                data = {'source': '0',
+                        'collectionId': path,
+                        'navRoot': '',
+                        'criteria': 'title',
+                        'sakai_action': 'doNavigate',
+                        'rt_action': '',
+                        'selectedItemId': '',
+                        'itemHidden': "false",
+                        'itemCanRevise': 'false',
+                        'sakai_csrf_token': sakai_csrf_token
+                        }
+                urlNew = r.html.find("form", first=True).attrs.get("action")
+                dirName = res.find("span.hidden-sm", first=True).text.strip()
+                dirName = re.sub(r"[/\\:*\"<>|?]", "", dirName)
+                dirName = os.path.join(currentClass, dirName)
+                getClass(dirName, urlNew, session, data)
         # 有版权的文件，构造下载链接
-        elif res.find("h4").a.get("href") == "#":
-            jsStr = res.find("h4").a.get("onclick")
+        elif res.find("a", first=True).attrs.get("href") == "#":
+            jsStr = res.find("a", first=True).attrs.get("onclick")
             reg = re.compile(r"openCopyrightWindow\('(.*)','copyright")
             match = reg.match(jsStr)
             if match:
                 resUrl = match.group(1)
-                resName = resUrl.split("/")[-1]
+                path = re.sub("http://course.ucas.ac.cn/access", "", resUrl)
+                resName = res.find("a")[1].text.replace("©", "").strip()
+                resName = re.sub(r"[/\\:*\"<>|?]", "", resName)
+                resUrl = "http://course.ucas.ac.cn/access/accept?ref=%s&url=%s" % (path, path)
                 download(resUrl, resName, currentClass, session)
         # 课件可以直接下载的
         else:
-            resName = resUrl.split("/")[-1]
+            resName = res.find("span.hidden-sm", first=True).text.strip()
+            resName = re.sub(r"[/\\:*\"<>|?]", "", resName)
+            if "." not in resName:
+                resName += (".%s" % resUrl.split(".")[-1])
             download(resUrl, resName, currentClass, session)
 
 
@@ -90,31 +104,30 @@ def getClassList(session):
 
     print("下载课程列表中....\n")
 
-    s = session.get("http://sep.ucas.ac.cn/portal/site/226/821")
-    bsObj = BeautifulSoup(s.text, "html.parser")
+    r = session.get("http://sep.ucas.ac.cn/portal/site/226/821")
 
-    url = bsObj.find("noscript").meta.get("content")[6:]
-    s = session.get(url)
+    url = r.html.find("noscript", first=True).find("meta", first=True).attrs.get("content")[6:]
+    r = session.get(url)
 
     terms = {}
-    s = session.get('http://jwxk.ucas.ac.cn/course/termSchedule')
-    soup = BeautifulSoup(s.text, "html.parser").find("select", {'name': 'termId'}).findAll("option")[:3]
-    for option in soup:
-        terms[option.get("value")] = option.get_text()
+    r = session.get('http://jwxk.ucas.ac.cn/course/termSchedule')
+    options = r.html.find("select[name=termId]", first=True).find("option")[:3]
+    for option in options:
+        terms[option.attrs.get("value")] = option.text
 
     classList = []
     for termId, termName in terms.items():
         post = {'termId': termId, 'courseAttribute': '', 'isSummer': ''}
-        s = session.post('http://jwxk.ucas.ac.cn/course/termSchedule', data=post)
-        bsObj = BeautifulSoup(s.text, "html.parser")
+        r = session.post('http://jwxk.ucas.ac.cn/course/termSchedule', data=post)
+        trs = r.html.find("tbody", first=True).find("tr")
 
-        for tr in bsObj.find("tbody").findAll("tr"):
-            tds = tr.findAll("td")
+        for tr in trs:
+            tds = tr.find("td")
             if len(tds) < 5: continue
-            no = tds[1].a.get("href")[19:]
-            id = tds[1].get_text()
-            name = tds[2].get_text()
-            teacher = tds[14].get_text()
+            no = tds[1].find("a", first=True).attrs.get("href")[19:]
+            id = tds[1].text
+            name = tds[2].text
+            teacher = tds[14].text
             classList.append("%s|%s|%s|%s|%s\n" % (no, id, name, teacher, termName))
 
     with open("classes.txt", "w", encoding="utf-8") as file:
@@ -129,18 +142,15 @@ def readClassList():
     return classList
 
 
-def addCourseSite(session, id):
-    s = session.get("http://sep.ucas.ac.cn/portal/site/226/821")
-    bsObj = BeautifulSoup(s.text, "html.parser")
-
-    url = bsObj.find("noscript").meta.get("content")[6:]
-    s = session.get(url)
-
-    s = session.get("http://jwxk.ucas.ac.cn/courseManage/addCourseSite.json?courseId=%s&_=%s" % (id, time.time()))
-    if "加入课程网站成功" in s.text:
-        return 1
-    else:
-        return s.text
+def addCourseSite(session, ids):
+    for id in ids:
+        r = session.get("http://sep.ucas.ac.cn/portal/site/226/821")
+        url = r.html.find("noscript", first=True).find("meta", first=True).attrs.get("content")[6:]
+        r = session.get(url)
+        r = session.get("http://jwxk.ucas.ac.cn/courseManage/addCourseSite.json?courseId=%s&_=%s" % (id, time.time()))
+        if not "加入课程网站成功" in r.text:
+            return r.text
+    return 1
 
 
 if __name__ == '__main__':
@@ -167,7 +177,7 @@ if __name__ == '__main__':
     if flag != "Y" and flag != "y":
         exit()
     try:
-        session = requests.Session()
+        session = HTMLSession()
         session.get("http://onestop.ucas.ac.cn/home/index")
         post = {'username': username, 'password': password, 'remember': 'checked'}
         headers = {'Host': 'onestop.ucas.ac.cn',
@@ -175,25 +185,22 @@ if __name__ == '__main__':
                    'X-Requested-With': 'XMLHttpRequest',
                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0'
                    }
-        s = session.post(
-            "http://onestop.ucas.ac.cn/Ajax/Login/0", data=post, headers=headers);
+        r = session.post(
+            "http://onestop.ucas.ac.cn/Ajax/Login/0", data=post, headers=headers)
 
-        # print(s.text)
-
-        if not "true" in s.text:
-            if "false" in s.text:
-                errorExit(json.loads(s.text)['msg'])
+        if not "true" in r.text:
+            if "false" in r.text:
+                errorExit(json.loads(r.text)['msg'])
             else:
-                errorExit(s.text)
+                errorExit(r.text)
 
-        url = json.loads(s.text)['msg']
-        s = session.get(url)
+        url = json.loads(r.text)['msg']
+        r = session.get(url)
 
-        bsObj = BeautifulSoup(s.text, "html.parser")
-        nameTag = bsObj.find("li", {"class": "btnav-info", "title": "当前用户所在单位"})
+        nameTag = r.html.find("li.btnav-info[title=当前用户所在单位]", first=True)
         if nameTag == None:
             errorExit("登录失败，请核对用户名密码、验证码")
-        name = nameTag.get_text()
+        name = nameTag.text
         # 正则提取出 姓名 （单位还是提取不出来啊 不知道为啥）
         match = re.compile(r"\s*(\S*)\s*(\S*)\s*").match(name)
         print("\n")
@@ -228,64 +235,77 @@ if __name__ == '__main__':
                     for c in addClassList:
                         print("%s %s %s %s %s" % (c[0], c[1], c[2], c[3], c[4]))
                         classNo.append(c[0])
-                    result = input("\n请输入要加入课程网站的课程的数字id：")
-                    if result in classNo:
-                        flag = addCourseSite(session, result)
-                        if flag == 1:
-                            errorExit("加入课程网站成功\n")
-                        else:
-                            errorExit(flag)
+                    result = input("\n请输入要加入课程网站的课程的数字id（可用逗号分隔）：")
+                    if "," in result:
+                        result = result.split(",")
                     else:
-                        errorExit("输入错误！\n")
+                        result = [result]
+
+                    for id in result:
+                        if id not in classNo:
+                            errorExit("输入错误！\n")
+
+                    flag = addCourseSite(session, result)
+                    if flag == 1:
+                        errorExit("%s个课程加入课程网站成功\n" % len(result))
+                    else:
+                        errorExit(flag)
+
                 # 待选课程为1 直接加入课程网站
                 else:
-                    flag = addCourseSite(session, addClassList[0][0])
+                    flag = addCourseSite(session, [addClassList[0][0]])
                     if flag == 1:
-                        errorExit("加入课程网站成功\n")
+                        errorExit("%s 加入课程网站成功\n" % addClassList[0][2])
                     else:
                         errorExit(flag)
             else:
                 errorExit("输入参数错误")
 
         # 课程网站
-        s = session.get("http://sep.ucas.ac.cn/portal/site/16/801")
-        bsObj = BeautifulSoup(s.text, "html.parser")
+        r = session.get("http://sep.ucas.ac.cn/portal/site/16/801")
 
-        newUrl = bsObj.find("noscript").meta.get("content")[6:]
-        s = session.get((newUrl))
-        bsObj = BeautifulSoup(s.text, "html.parser")
-        newUrl = "http://course.ucas.ac.cn" + bsObj.find("frameset").findAll("frame")[0].get("src")
-        s = session.get(newUrl)
-        bsObj = BeautifulSoup(s.text, "html.parser").find("a", {"class": "icon-sakai-membership"})
-        newUrl = bsObj.get("href")
-        # 进入我的课程页面
-        s = session.get(newUrl)
-        newUrl = BeautifulSoup(s.text, "html.parser").find("iframe").get("src");
-        # 进入课程列表
-        s = session.get(newUrl)
+        newUrl = r.html.find("noscript", first=True).find("meta", first=True).attrs.get("content")[6:]
+        # http://course.ucas.ac.cn/portal/plogin?Identity=fd86ee9b-3e45-4e5f-9bd1-5647c652221f&roleId=801
+        r = session.get(newUrl)
+
+        newUrl = r.html.find("a#allSites", first=True).attrs.get("href")
+        # 右上角“我的课程”->查看所有课程
+        # http://course.ucas.ac.cn/portal/site/%7E201628018629055/tool-reset/7047982f-9573-4de6-ab03-8357bdb1ef53
+        r = session.get(newUrl)
+        # hidden
+        sakai_csrf_token = r.html.find("input[name=sakai_csrf_token]", first=True).attrs.get("value")
+        post = {
+            "eventSubmit_doChange_pagesize": "changepagesize",
+            "selectPageSize": 200,
+            "sakai_csrf_token": sakai_csrf_token
+        }
+        # http://course.ucas.ac.cn/portal/site/~201628018629055/tool/7047982f-9573-4de6-ab03-8357bdb1ef53
+        # 每页显示200个课程
+        newUrl = re.sub("-reset", "", newUrl)
+        r = session.post(newUrl, data=post)
+
+        titles = r.html.find("td[headers=title]")
+        # 课程列表 去掉第一行
         # 读取所有课程
         classList = []
-        trList = BeautifulSoup(s.text, "html.parser").findAll("tr")
-        # 去掉第一行
-        del trList[0]
-        for tr in trList:
-            tdList = tr.findAll("td")
-            className = tdList[2].get_text().strip()  # 课程名
-            classId = tdList[0].get_text().strip()  # 课程id
-            classWebsite = tdList[2].h4.a.get("href")  # 课程url
-            classTeacher = tdList[3].get_text().strip()  # 课程老师
-            classList.append((classId, className, classWebsite, classTeacher));
+        for title in titles[1:]:
+            className = title.text.strip()
+            className = re.sub(r"[/\\:*\"<>|?]", "", className)
+            classId = title.find("a.getSiteDesc", first=True).attrs.get("id")
+            classWebsite = title.find("a", first=True).attrs.get("href")
+            classList.append((classId, className, classWebsite))
+
         print("您已选%s门课：" % str(len(classList)))
         # 打印所有课程
         for c in classList:
-            print(c[1] + "(" + c[3] + ")")
+            print(c[1])
 
         downloadClassList = []
         # 下载指定课程的课件
         if len(sys.argv) > 1:
             selectedClass = sys.argv[1]
             for c in classList:
-                if selectedClass in c[1] or selectedClass in c[0] or selectedClass in c[3]:
+                if selectedClass in c[1] or selectedClass in c[0]:
                     downloadClassList.append(c)
         if len(downloadClassList) > 0:
             classList = downloadClassList[:]
@@ -295,13 +315,12 @@ if __name__ == '__main__':
         print("开始下载课件......")
         for c in classList:
             url = c[2]
-            s = session.get(url)
-            # 资源url
-            url = BeautifulSoup(s.text, "html.parser").find("a", {"class", "icon-sakai-resources"}).get("href")
-            s = session.get(url)
-            url = BeautifulSoup(s.text, "html.parser").find("iframe").get("src")
+            r = session.get(url)
+            # 左侧菜单栏 第4个 ”资源“
+            url = r.html.find("a.Mrphs-toolsNav__menuitem--link")[3].attrs.get("href")
+            print("\n=====%s=====" % c[1])
             getClass(c[1], url, session, None)
-    except Exception as e:
+    except NameError as e:
         errorExit("妈呀，出错了，错误信息: %s" % e)
 
     print("\n")
